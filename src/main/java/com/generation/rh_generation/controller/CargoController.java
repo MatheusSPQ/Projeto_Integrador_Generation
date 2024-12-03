17952fb40c2b275948870633294948a6a7984cb6
Jaque <jaquelinecosta-rj@hotmail.com>
2024-12-03 14:50:01 -0300

    Descrição do que foi alterado

--- src/main/java/com/generation/rh_generation/controller/CargoController.java
+++ src/main/java/com/generation/rh_generation/controller/CargoController.java
@@ -1,10 +1,17 @@
 package com.generation.rh_generation.controller;
 
 
+import com.generation.rh_generation.model.Cargo;
 import com.generation.rh_generation.repository.CargoRepository;
 import com.generation.rh_generation.repository.FuncionarioRepository;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +25,18 @@ public class CargoController {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCargo(@PathVariable Long id) {
+    	Optional<Cargo> cargoOptional = cargoRepository.findById(id);
+    	if (cargoOptional.isPresent()) {
+    		cargoRepository.deleteById(id);
+    		return ResponseEntity.ok("Cargo deletado com sucesso.");
+    	}  else  {
+    		return ResponseEntity.status(404).body("Cargo não encontrado.");
+    	}
+    	}
+    }
 
 
 
@@ -27,4 +46,4 @@ public class CargoController {
 
 
 
-}
+
--- src/main/java/com/generation/rh_generation/controller/FuncionarioController.java
+++ src/main/java/com/generation/rh_generation/controller/FuncionarioController.java
@@ -1,9 +1,16 @@
 package com.generation.rh_generation.controller;
 
+import com.generation.rh_generation.model.Funcionario;
 import com.generation.rh_generation.repository.CargoRepository;
 import com.generation.rh_generation.repository.FuncionarioRepository;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +24,16 @@ public class FuncionarioController {
 
     @Autowired
     CargoRepository cargoRepository;
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteFuncionario(@PathVariable Long id) {
+    	Optional<Funcionario> funcionarioOptional = funcionarioRepository.findById(id);
+    	if (funcionarioOptional.isPresent()) {
+    		funcionarioRepository.deleteById(id);
+    		return ResponseEntity.ok("Funcionário deletado com sucesso.");
+    	} else {
+    		return ResponseEntity.status(404).body("Funcionário não encontrado.");
+    	}
+    	
+    }
 }
