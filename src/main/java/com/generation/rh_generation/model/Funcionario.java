9585e2f291ff89f332373db236c5862b9ccd705c
Matheus <matheuspq11@gmail.com>
2024-12-10 10:12:33 -0300

    add entity cargo/controller/repository

--- src/main/java/com/generation/rh_generation/model/Funcionario.java
+++ src/main/java/com/generation/rh_generation/model/Funcionario.java
@@ -1,6 +1,7 @@
 package com.generation.rh_generation.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -27,15 +29,13 @@ public class Funcionario {
     @Column(nullable = false, unique = true)
     private String cpf;
 
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
 
     @Temporal(TemporalType.DATE)
     @Column(nullable = false)
-    private Date dataNascimento;
-
-
-    @Temporal(TemporalType.DATE)
-    @Column(nullable = false)
-    private Date dataAdmissao;
+    private LocalDate dataAdmissao;
 
     @NotBlank
     @Size(min = 11, max = 11)
@@ -45,62 +45,85 @@ public class Funcionario {
     @Email
     private String email;
 
+    @ManyToOne
+    @JsonIgnoreProperties("funcionario")
+    private Cargo cargo;
+
 
     public long getId() {
         return id;
     }
 
     public void setId(long id) {
+
         this.id = id;
     }
 
     public @NotBlank @Size(min = 3, max = 150) String getNome() {
+
         return nome;
     }
 
     public void setNome(@NotBlank @Size(min = 3, max = 150) String nome) {
+
         this.nome = nome;
     }
 
     public @NotBlank @CPF String getCpf() {
+
         return cpf;
     }
 
     public void setCpf(@NotBlank @CPF String cpf) {
+
         this.cpf = cpf;
     }
 
-    public Date getDataNascimento() {
+    public LocalDate getDataNascimento() {
+
         return dataNascimento;
     }
 
-    public void setDataNascimento( Date dataNascimento) {
+    public void setDataNascimento( LocalDate dataNascimento) {
+
         this.dataNascimento = dataNascimento;
     }
 
-
-    public  Date getDataAdmissao() {
-
+    public LocalDate getDataAdmissao() {
         return dataAdmissao;
     }
 
-    public void setDataAdmissao( Date dataAdmissao) {
+    public void setDataAdmissao( LocalDate dataAdmissao) {
+
         this.dataAdmissao = dataAdmissao;
     }
 
     public @NotBlank @Size(min = 11, max = 11) String getTelefone() {
+
         return telefone;
     }
 
     public void setTelefone(@NotBlank @Size(min = 11, max = 11) String telefone) {
+
         this.telefone = telefone;
     }
 
     public @NotBlank @Email String getEmail() {
+
         return email;
     }
 
     public void setEmail(@NotBlank @Email String email) {
+
         this.email = email;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+
+        this.cargo = cargo;
     }
 }
--- src/main/java/com/generation/rh_generation/repository/CargoRepository.java
+++ src/main/java/com/generation/rh_generation/repository/CargoRepository.java
@@ -1,14 +1,19 @@
 package com.generation.rh_generation.repository;
 
 import com.generation.rh_generation.model.Cargo;
-import com.generation.rh_generation.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
 @Repository
-public interface FuncionarioRepository  extends JpaRepository<Funcionario, Long> {
-    public List<Funcionario> findByCpf(@Param("cpf") String cpf);
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+
+    public List<Cargo> findByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
+
+
+
